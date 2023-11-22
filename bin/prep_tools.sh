1f57e14b4bc38342a259bdd34f1c00117746dad2
Valentine Shatravenko <vshatravenko@gmail.com>
2023-11-22 17:44:19 -0500

    fix: make prep_tools.sh quieter

--- bin/prep_tools.sh
+++ bin/prep_tools.sh
@@ -6,23 +6,23 @@ HELM_VERSION=3.12.2
 RCLONE_VERSION=current
 
 main() {
-	sudo apt update -q
-	sudo apt install -y curl unzip
+	sudo apt update -qq
+	sudo apt install -yq curl unzip
 
 	install_rclone
 	install_helm
 }
 
 install_rclone() {
-	curl -o rclone.zip https://downloads.rclone.org/rclone-${RCLONE_VERSION}-linux-amd64.zip
-	unzip rclone.zip
+	curl -so rclone.zip https://downloads.rclone.org/rclone-${RCLONE_VERSION}-linux-amd64.zip
+	unzip -q rclone.zip
 	chmod +x rclone-*-linux-amd64/rclone
 	sudo mv rclone-*-linux-amd64/rclone /usr/local/bin
 }
 
 
 install_helm() {
-	curl -o helm.tar.gz "https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+	curl -so helm.tar.gz "https://get.helm.sh/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 	tar xzf helm.tar.gz
 	chmod +x linux-amd64/helm
 	sudo mv linux-amd64/helm /usr/local/bin
